1b9dd5f1fc60190285396416b216ab0611be1d6f
sumanth <sumanth.ece52@gmail.com>
2019-12-28 21:10:14 +0530

    removed old assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BlogComponent } from './blog/blog.component';
 import {HomeComponent} from "./home/home.component";
-import {PostComponent} from "./post/post.component";
 
 
 const routes: Routes = [
-  {
-    path: 'blog/:id/:name',
-    component: BlogComponent
-  },
-  {
-    path: 'post/:id',
-    component: PostComponent
-  },
   {
     path: '',
     component: HomeComponent
